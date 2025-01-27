327d9edb213efb79fea9e099ea9f04e7d9d3ba46
BigRedBigTractor <79341830+BigRedBigTractor@users.noreply.github.com>
2025-01-26 16:24:39 -0800

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/*
+{ This lab requires students to 1) set up an Integrated Development Environment (IDE), 2) create a Github account for version control 3) familiarize with Command Line Interface (CLI), and 4) compile and execute C++ programs.}
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std; 
+
+int main(){
+    cout << "Hello World!" << endl; 
+    printf("Hello World again!\n");
+    return 0; 
+}
